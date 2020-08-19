8d680c308a7c166ce241265e1fc9c76066018d19
ahsantawil <ahsan.tawil07@gmail.com>
2020-08-19 16:46:25 +0700

    add schema user and movie

---
+++ models/userSchema.js
@@ -0,0 +1,30 @@
+const mongoose  = require('mongoose');
+const bcrypt    = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type: String,
+        required: true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    Date : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+userSchema.pre('save', function(next){
+    const user = this;
+    user.password = bcrypt.hashSync(user.password, 10);
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
