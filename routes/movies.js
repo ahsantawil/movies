6955f06fee2b630c0588490503ea112c8c8ec67b
ahsantawil <ahsan.tawil07@gmail.com>
2020-08-22 15:06:43 +0700

    get, create, crate dashboard and header

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,31 @@
-const express   = require('express');
+const express    = require('express');
 const { render } = require('ejs');
 
 const router    = express.Router();
+const Movie     = require('../models/movieSchema');
 
 // Get All Movies
 router.get('/', function(req, res, next){
-    res.render('movie/allMovies', {
-        title:'Get Movies'
-    })
+    let listMovies = [];
+    Movie.find(function(err, movies){
+        if (movies) {
+            for (let data of movies) {
+                listMovies.push({
+                    id: data._id,
+                    name : data.name,
+                    release_on:data.release_on
+                });
+            }
+            res.render('movie/allMovies',{listMovies});
+            } else{
+                listMovies.push({
+                    id:'',
+                    name:'',
+                    release_on:''
+                });
+                res.render('movie/allMovies',{listMovies});
+            }
+    });
 });
 
 // Create Movies
@@ -27,7 +45,25 @@ router.get('/update/:movieId', function(req, res, next) {
 
 //Action create
 router.post('/create', function(req, res){
-
+    const {name, date} = req.body;
+    
+    let errors = [];
+    if (!name || !date) {
+        errors.push({msg:'Silahkan lengkapi data yang dibutuhkan'});
+    }
+    if (errors.length > 0) {
+        res.render('movie/createMovies', {errors});
+    } else {
+        const movie = Movie({
+            name,
+            release_on : date
+        });
+        movie.save().then(movie => {
+            errors.push({msg:'Data movie berhasil ditambah'});
+            res.render('movie/createMovies',{errors});
+            }
+        ).catch(err=>console.log(err));
+    }
 });
 
 //Action Update
